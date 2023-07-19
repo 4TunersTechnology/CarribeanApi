6ac591862b68b0d1823cae54ce663511ba34f0be
Lakhan_kanade <www.lkanade@gmail.com>
2023-07-19 15:41:49 +0530

    signup Api

---
+++ Index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const mongoose = require('mongoose')
+const bodyParser=require('body-parser')
+const cors =require('cors')
+const Users =require('./SignupModule/Signupmodules')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+app.use(express.json());
+app.use(cors())
+
+const url = `mongodb://4tuners:12345@ac-qnxikpe-shard-00-00.lranesa.mongodb.net:27017,ac-qnxikpe-shard-00-01.lranesa.mongodb.net:27017,ac-qnxikpe-shard-00-02.lranesa.mongodb.net:27017/Carribean?ssl=true&replicaSet=atlas-yjmn84-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+mongoose.connect(url)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+    app.post("/auth/signup", async (req, res) => {
+        const { email, password, fullName } = req.body;
+      
+        // const encrypt_password = await bcrypt.hash(password, 10);
+      
+        const userDetail = {
+          email: email,
+          password: password,
+          fullName: fullName,
+        };
+      
+        const user_exist = await Users.findOne({ email: email });
+      
+        if (user_exist) {
+            
+            
+          res.send({ message: "The Email is already in use !" });
+        } else {
+            console.log('full name ',fullName , email , password)
+            if(fullName == undefined || fullName ==""){
+                res.send({message:'Full name Required'})
+            }
+            else if(email == undefined || email ==""){
+                res.send({message:'Email invalide'})
+            }
+            else if(password == undefined || password ==""){
+                res.send({message:'password Required'})
+            }
+            else{
+          Users.create(userDetail, (err, result) => {
+            if (err) {
+              res.status(500).send({ message: err.message });
+            } else {
+              res.send({ message: "User Created Succesfully" });
+            }
+          });
+        }
+        }
+      });
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
